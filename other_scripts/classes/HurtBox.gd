e97823edb59bf590cdcce792a02bf791715643ce
SamuelRodriguesMGR <marat78880@gmail.com>
2025-01-16 12:19:46 +0500

    исправление ошибок в консоли

--- other_scripts/classes/HurtBox.gd
+++ other_scripts/classes/HurtBox.gd
@@ -5,11 +5,9 @@ func _init() -> void:
 	collision_layer = 4
 	collision_mask = 3
 
-func _ready() -> void:
-	self.area_entered.connect(_on_hit)
-
 
 func _on_hit(hitbox: HitBox) -> void:
+	print(hitbox)
 	if hitbox == null:
 		return
 	
@@ -17,3 +15,8 @@ func _on_hit(hitbox: HitBox) -> void:
 	if hitbox.get_parent().name != owner.name:
 		if owner.has_method("TakeDamage") != null:
 			owner.call("TakeDamage", hitbox.damage)
+
+
+func _on_area_entered(area: Area3D) -> void:
+	if area in get_tree().get_nodes_in_group(&"HitBox"):
+		_on_hit(area)
